68b1955f92b3b58aa1b2269532b060509d8c4b1e
Roland Gjoka <rolandgjoka@hotmail.com>
2020-12-11 13:21:47 -0600

    bug fix

---
+++ public/js/choices.js
@@ -0,0 +1,15 @@
+function getRest () {
+        $.ajax({
+      url: `https://developers.zomato.com/api/v2.1/search?count=20&lat=${lat}&lon=%20${lng}`,
+      headers: {
+        'user-key': '31549f18a4409b92a3243bf922d7853a',
+        Accept: 'application/json'
+      }
+    }).then((restaurants) => {
+      const restName = restaurants.restaurants[0].restaurant.name      
+      let restThumb = restaurants.restaurants[0].restaurant.thumb     
+      if (restThumb.length < 1) {
+        restThumb = 'https://via.placeholder.com/250x250'
+      }
+    })
+  }
