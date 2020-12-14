8249db28c9bdfd7c060b8bf782d16b9b0e3fc95e
Greyson Kirk <greysonkirk@yahoo.com>
2020-12-14 14:51:43 -0600

    changed interval

--- public/js/choices.js
+++ public/js/choices.js
@@ -1,53 +1,53 @@
 getRest()
 const yumBtn = $('#yum')
 
-function getRest() {
-    // const lat = 30.326374
-    // const lng = -97.771258
-    $.ajax({
-        url: '/api/zomato/30.326374/-97.771258'
-    }).then((restaurants) => {
-        const choices = []
-        console.log(restaurants)
-        for (let i = 0; i < restaurants.restaurants.length; i++) {
-            const restId = restaurants.restaurants[i].restaurant.id
-            const restName = restaurants.restaurants[i].restaurant.name
-            let restThumb = restaurants.restaurants[i].restaurant.thumb
-            const restAddress = restaurants.restaurants[i].restaurant.location.address
-            const restHours = restaurants.restaurants[i].restaurant.timings
-            const restRating = restaurants.restaurants[i].restaurant.user_rating.rating_text
-            const restPhone = restaurants.restaurants[i].restaurant.phone_numbers
-            const ratingColor =
+function getRest () {
+  // const lat = 30.326374
+  // const lng = -97.771258
+  $.ajax({
+    url: '/api/zomato/30.326374/-97.771258'
+  }).then((restaurants) => {
+    const choices = []
+    console.log(restaurants)
+    for (let i = 0; i < restaurants.restaurants.length; i++) {
+      const restId = restaurants.restaurants[i].restaurant.id
+      const restName = restaurants.restaurants[i].restaurant.name
+      let restThumb = restaurants.restaurants[i].restaurant.thumb
+      const restAddress = restaurants.restaurants[i].restaurant.location.address
+      const restHours = restaurants.restaurants[i].restaurant.timings
+      const restRating = restaurants.restaurants[i].restaurant.user_rating.rating_text
+      const restPhone = restaurants.restaurants[i].restaurant.phone_numbers
+      const ratingColor =
                 restaurants.restaurants[i].restaurant.user_rating.rating_color
-            const restMenu = restaurants.restaurants[i].restaurant.menu_url
-            const restCuisines = restaurants.restaurants[0].restaurant.cuisines
-            if (restThumb.length < 1) {
-                restThumb = 'https://via.placeholder.com/250x250'
-            }
-            choices.push({
-                restId,
-                restName,
-                restThumb,
-                restAddress,
-                restHours,
-                restRating,
-                restPhone,
-                restMenu,
-                ratingColor,
-                restCuisines
-            })
-        }
-        console.log(choices)
-        buildSwipe(choices)
-    })
+      const restMenu = restaurants.restaurants[i].restaurant.menu_url
+      const restCuisines = restaurants.restaurants[0].restaurant.cuisines
+      if (restThumb.length < 1) {
+        restThumb = 'https://via.placeholder.com/250x250'
+      }
+      choices.push({
+        restId,
+        restName,
+        restThumb,
+        restAddress,
+        restHours,
+        restRating,
+        restPhone,
+        restMenu,
+        ratingColor,
+        restCuisines
+      })
+    }
+    console.log(choices)
+    buildSwipe(choices)
+  })
 }
 
-function buildSwipe(choices) {
-    for (let i = 0; i < choices.length; i++) {
-        let active = ''
-        if (i === 0) { active = 'active' }
-        console.log()
-        const swipe = `<div id="restDisplay" class="carousel-item ${active}" data-bs-touch="true" data-value="${choices[i].restId} data-bs-interval="false"">
+function buildSwipe (choices) {
+  for (let i = 0; i < choices.length; i++) {
+    let active = ''
+    if (i === 0) { active = 'active' }
+    console.log()
+    const swipe = `<div id="restDisplay" class="carousel-item ${active}" data-bs-touch="true" data-value="${choices[i].restId} data-bs-interval="false"">
       <img src='${choices[i].restThumb}' class="d-block w-100">
       <div class="restName"><span>${choices[i].restName}</span></div>
       <p>${choices[i].restCuisines} <span class="rounded" style="background-color:#${choices[i].ratingColor}; padding-left: 3px;padding-right: 3px;"; font-size: 20px;"> ${choices[i].restRating}</span></p>
@@ -56,20 +56,20 @@ function buildSwipe(choices) {
       <p><a id="icon" href="tel:${choices[i].restPhone}">${choices[i].restPhone}<a/></p>
       <p><a href="${choices[i].restMenu}" target="_blank">Menu</a></p>
   </div>`
-        $(swipe).appendTo('.carousel-inner')
-    }
+    $(swipe).appendTo('.carousel-inner')
+  }
 }
 yumBtn.on('click', event => {
-    event.preventDefault()
-    const restChoice = $('.active').data('value')
-    console.log(restChoice)
-    $.ajax({
-            url: `/api/zomato/${restChoice}`,
-            method: 'POST',
-            data: jQuery.param(restChoice)
+  event.preventDefault()
+  const restChoice = $('.active').data('value')
+  console.log(restChoice)
+  $.ajax({
+    url: `/api/zomato/${restChoice}`,
+    method: 'POST',
+    data: jQuery.param(restChoice)
 
-        })
-        .then((data) => {
-            console.log(data)
-        })
+  })
+    .then((data) => {
+      console.log(data)
+    })
 })
