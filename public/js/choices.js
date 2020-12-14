e74b6ecc169c191396d84eb3be31e23e0ae491ea
Roland Gjoka <rolandgjoka@hotmail.com>
2020-12-14 12:39:24 -0600

    adding carouse

--- public/js/choices.js
+++ public/js/choices.js
@@ -1,5 +1,4 @@
 getRest()
-const choices = []
 
 function getRest () {
   // const lat = 30.326374
@@ -7,8 +6,10 @@ function getRest () {
   $.ajax({
     url: '/api/zomato/30.326374/-97.771258'
   }).then((restaurants) => {
+    const choices = []
     console.log(restaurants)
     for (let i = 0; i < restaurants.restaurants.length; i++) {
+      const restId = restaurants.restaurants[i].restaurant.id
       const restName = restaurants.restaurants[i].restaurant.name
       let restThumb = restaurants.restaurants[i].restaurant.thumb
       const restAddress = restaurants.restaurants[i].restaurant.location.address
@@ -23,6 +24,7 @@ function getRest () {
         restThumb = 'https://via.placeholder.com/250x250'
       }
       choices.push({
+        restId,
         restName,
         restThumb,
         restAddress,
@@ -35,24 +37,24 @@ function getRest () {
       })
     }
     console.log(choices)
-    buildSwipe()
+    buildSwipe(choices)
   })
 }
 
-function buildSwipe () {
+function buildSwipe (choices) {
   for (let i = 0; i < choices.length; i++) {
-    const swipe = `<div class="swiper-slide"><img class="restImg" src='${choices[i].restThumb}'></img>
-        <div class="restName"><span>${choices[i].restName}</span></div>
-        <p>${choices[i].restCuisines} <span class="rounded" style="background-color:#${choices[i].ratingColor}; padding-left: 3px;padding-right: 3px;"; font-size: 20px;"> ${choices[i].restRating}</span></p>
-        <p>${choices[i].restAddress}</p>
-        <p>${choices[i].restHours}</p>
-        <p><a id="icon" href="tel:${choices[i].restPhone}">${choices[i].restPhone}<a/></p>
-        <p><a href="${choices[i].restMenu}" target="_blank">Menu</a></p>
-        <div class="buttons text-center">
-            <div id="yum" class="btn btn-success btn-hover">Yummy!</div>
-            <div id="yuck" class="btn btn-danger btn-hover">Yuck!</div>
-        </div>
-    </div>`
-    $(swipe).appendTo('.swiper-wrapper')
+    let active = ''
+    if (i === 0) { active = 'active' }
+    console.log(i)
+    const swipe = `<div class="carousel-item ${active}" data-bs-touch="true" value="${choices[i].restId}">
+      <img src='${choices[i].restThumb}' class="d-block w-100">
+      <div class="restName"><span>${choices[i].restName}</span></div>
+      <p>${choices[i].restCuisines} <span class="rounded" style="background-color:#${choices[i].ratingColor}; padding-left: 3px;padding-right: 3px;"; font-size: 20px;"> ${choices[i].restRating}</span></p>
+      <p>${choices[i].restAddress}</p>
+      <p>${choices[i].restHours}</p>
+      <p><a id="icon" href="tel:${choices[i].restPhone}">${choices[i].restPhone}<a/></p>
+      <p><a href="${choices[i].restMenu}" target="_blank">Menu</a></p>
+  </div>`
+    $(swipe).appendTo('.carousel-inner')
   }
 }
