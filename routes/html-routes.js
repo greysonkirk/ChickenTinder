3d8c7a36df61f68d4fa4dc31c00e8eaf81dbf4f1
Greyson Kirk <greysonkirk@yahoo.com>
2020-12-15 09:11:35 -0600

    add match btn

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -4,39 +4,39 @@
 // Requiring our custom middleware for checking if a user is logged in
 const isAuthenticated = require('../config/isAuthenticated')
 
-module.exports = function(app) {
-    app.get('/', (req, res) => {
-        // If the user already has an account send them to the members page
+module.exports = function (app) {
+  app.get('/', (req, res) => {
+    // If the user already has an account send them to the members page
 
-        res.render('index')
-    })
-    app.get('/signup', (req, res) => {
-        // If the user already has an account send them to the members page
-        if (req.user) {
-            res.render('members')
-        }
-        res.render('signup')
-    })
+    res.render('index')
+  })
+  app.get('/signup', (req, res) => {
+    // If the user already has an account send them to the members page
+    if (req.user) {
+      res.render('members')
+    }
+    res.render('signup')
+  })
 
-    app.get('/login', (req, res) => {
-        // If the user already has an account send them to the members page
-        if (req.user) {
-            res.render('members')
-        }
-        res.render('login')
-    })
+  app.get('/login', (req, res) => {
+    // If the user already has an account send them to the members page
+    if (req.user) {
+      res.render('members')
+    }
+    res.render('login')
+  })
 
-    // Here we've add our isAuthenticated middleware to this route.
-    // If a user who is not logged in tries to access this route they will be redirected to the signup page
-    app.get('/members', isAuthenticated, (req, res) => {
-        res.render('members')
-    })
+  // Here we've add our isAuthenticated middleware to this route.
+  // If a user who is not logged in tries to access this route they will be redirected to the signup page
+  app.get('/members', isAuthenticated, (req, res) => {
+    res.render('members')
+  })
 
-    app.get('/choices', isAuthenticated, (req, res) => {
-        res.render('choices')
-    })
+  app.get('/choices', isAuthenticated, (req, res) => {
+    res.render('choices')
+  })
 
-    app.get('/matched', isAuthenticated, (req, res) => {
-        res.render('matched')
-    })
+  app.get('/matched', isAuthenticated, (req, res) => {
+    res.render('matched')
+  })
 }
